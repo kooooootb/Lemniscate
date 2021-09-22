2c42443fd5b196e1602729cddb1e32f39bd9c9ee
Maksim Zavgorodny <zavgmaxden@mail.ru>
2021-09-22 17:45:53 +0300

    deleted cmake-build-debug

--- cmake-build-debug/lemniscate-tests/lib/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/zavgm/2Labs/lab2/lemniscate-tests/lib/googletest
-# Build directory: C:/Users/zavgm/2Labs/lab2/cmake-build-debug/lemniscate-tests/lib/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
